c8f8db33690bb32cf975cf051dba9d1c265220b3
JMjirapat <jm_jirapat@hotmail.com>
2024-06-04 12:35:04 +0700

    impl: Queue, Stack

--- ds.hs
+++ ds.hs
@@ -19,4 +19,28 @@ deleteLinkListed x (LinkNode y xs)
     | x == y    = xs
     | otherwise = LinkNode y (deleteLinkListed x xs)
 
-xList = LinkNode 1 (LinkNode 2 (LinkNode 3 LinkEmpty))
+type Stack a = [a]
+
+push :: a -> Stack a -> Stack a
+push = (:)
+
+pop :: Stack a -> (Maybe a, Stack a)
+pop [] = (Nothing, [])
+pop (x:xs) = (Just x, xs)
+
+searchStack :: (Eq a) => a -> Stack a -> Maybe a
+searchStack _ [] = Nothing
+searchStack x (y:ys) = if x == y then Just y else searchStack x ys
+
+type Queue a = [a]
+
+enqueue :: a -> Queue a -> Queue a
+enqueue x = (++[x])
+
+dequeue :: Queue a -> (Maybe a, Queue a)
+dequeue [] = (Nothing, [])
+dequeue (x:xs) = (Just x, xs)
+
+searchQueue :: (Eq a) => a -> Queue a -> Maybe a
+searchQueue _ [] = Nothing
+searchQueue x (y:ys) = if x == y then Just y else searchQueue x ys
